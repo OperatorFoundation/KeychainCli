74a2dcbdfb6fc5e337b67f7062b46b557048703d
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-09-17 15:21:02 -0500

    Added signing keys

--- Sources/Keychain/Keychain.swift
+++ Sources/Keychain/Keychain.swift
@@ -11,3 +11,24 @@ import Foundation
 @_exported import KeychainLinux
 
 #endif
+
+public protocol KeychainProtocol: Codable
+{
+    // Key Agreement
+    func generateAndSavePrivateKey(label: String) -> P256.KeyAgreement.PrivateKey?
+    func retrievePrivateKey(label: String) -> P256.KeyAgreement.PrivateKey?
+    func deleteKey(label: String)
+
+    func retrieveOrGeneratePrivateKey(label: String) -> P256.KeyAgreement.PrivateKey?
+    func storePrivateKey(_ key: P256.KeyAgreement.PrivateKey, label: String) -> Bool
+    func generateKeySearchQuery(label: String) -> CFDictionary
+
+    // Signing
+    func generateAndSavePrivateSigningKey(label: String) -> P256.Signing.PrivateKey?
+    func retrievePrivateSigningKey(label: String) -> P256.Signing.PrivateKey?
+
+    func retrieveOrGeneratePrivateSigningKey(label: String) -> P256.Signing.PrivateKey?
+    func storePrivateSigningKey(_ key: P256.Signing.PrivateKey, label: String) -> Bool
+}
+
+extension Keychain: KeychainProtocol {}
---
+++ Sources/Keychain/Keys.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dr. Brandon Wiley on 1/27/22.
+//
+
+import Foundation
+
