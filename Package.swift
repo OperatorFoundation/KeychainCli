19200d0e212fa67558cab91b8d51d86fbe384435
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-12-16 22:42:22 -0600

    Renamed package

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Keychain",
-            targets: ["Keychain"]),
+            name: "KeychainCli",
+            targets: ["KeychainCli"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,14 +24,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Keychain",
+            name: "KeychainCli",
             dependencies: [
                 "KeychainLinux",
                 "KeychainTypes",
             ]),
         .testTarget(
             name: "KeychainTests",
-            dependencies: ["Keychain", "KeychainLinux", "KeychainTypes",]),
+            dependencies: ["KeychainCli", "KeychainLinux", "KeychainTypes",]),
     ],
     swiftLanguageVersions: [.v5]
 )
