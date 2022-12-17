72e3119809866fd885a10acff90a2b205c963196
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-12-16 22:39:55 -0600

    Modified to work with SPM projects

--- Sources/Keychain/Keychain.swift
+++ Sources/Keychain/Keychain.swift
@@ -1,11 +1,2 @@
 @_exported import KeychainTypes
-
-#if os(macOS) || os(iOS)
-
-@_exported import KeychainMacOS
-
-#else
-
 @_exported import KeychainLinux
-
-#endif
--- Tests/KeychainTests/KeychainTests.swift
+++ Tests/KeychainTests/KeychainTests.swift
@@ -5,7 +5,13 @@ final class KeychainTests: XCTestCase
 {
     func testSaveAndLoad()
     {
-        let keychain = Keychain()
+        let url = URL(fileURLWithPath: "keychain")
+        guard let keychain = Keychain(baseDirectory: url) else
+        {
+            XCTFail()
+            return
+        }
+
         let key1 = keychain.generateAndSavePrivateKey(label: "test", type: KeyType.P256KeyAgreement)
         let key2 = keychain.retrieveOrGeneratePrivateKey(label: "test", type: KeyType.P256KeyAgreement)
 
